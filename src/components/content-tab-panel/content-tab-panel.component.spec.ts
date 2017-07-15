9dbb0486a473b464170466d5d6f1beca26ea3079
Phani Rahul Sivalenka <rahul.sivalenka@walkingtree.tech>
2017-07-15 14:45:24 +0530

    Fixed tab panel and navbar specs

--- src/components/content-tab-panel/content-tab-panel.component.spec.ts
+++ src/components/content-tab-panel/content-tab-panel.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MdTabsModule } from '@angular/material';
 
 import { ContentTabPanelComponent } from './content-tab-panel.component';
 
@@ -8,7 +10,11 @@ describe('ContentTabPanelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContentTabPanelComponent ]
+      declarations: [ ContentTabPanelComponent ],
+      imports: [
+        RouterTestingModule,
+        MdTabsModule
+      ]
     })
     .compileComponents();
   }));

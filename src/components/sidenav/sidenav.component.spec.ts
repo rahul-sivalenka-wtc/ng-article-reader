27b53067f3a90f659385ee53bf7efa62db61c341
Phani Rahul Sivalenka <rahul.sivalenka@walkingtree.tech>
2017-07-15 13:04:11 +0530

    - Modified sidenav specs - Modified app specs - Added a generic mock component

--- src/components/sidenav/sidenav.component.spec.ts
+++ src/components/sidenav/sidenav.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MdSidenavModule } from '@angular/material';
 
 import { SidenavComponent } from './sidenav.component';
+import { MockComponent } from '../../shared/mocks/mock.component';
 
-describe('SidenavComponent', () => {
+fdescribe('SidenavComponent', () => {
   let component: SidenavComponent;
   let fixture: ComponentFixture<SidenavComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidenavComponent ]
+      declarations: [ 
+        SidenavComponent,
+        MockComponent({ selector: 'ar-article-tree' }),
+        MockComponent({ selector: 'ar-admin-panel' })
+      ],
+      imports: [
+        MdSidenavModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
---
+++ src/shared/mocks/mock.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Reference: https://medium.com/@cnunciato/a-simple-mock-component-for-angular-2-d79bd58a7b31
+ * 
+ * Modified By:
+ * Phani Rahul Sivalenka
+ */
+
+import { Component } from '@angular/core';
+
+export function MockComponent(options: Component): any {
+  let metadata: Component = {
+    selector: options.selector,
+    template: options.template || '',
+    inputs: options.inputs,
+    outputs: options.outputs
+  };
+  return Component(metadata)(class _ {});
+}
